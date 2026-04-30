e9143a2f149c9e3ef1195146cce78d5208c997f2
TVTIT <trungtran200656@gmail.com>
2026-04-30 09:22:31 +0700

    thêm icon loading vào splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,39 +1,59 @@
 import 'package:electronic_component_storage_app/view/app_color.dart';
 import 'package:flutter/material.dart';
-// Note: Import file chứa màn hình tiếp theo (Login/Home) của em vào đây
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {    
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool _showLoader = false;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColor.surfaceColor, 
+      backgroundColor: AppColor.surfaceColor,
       body: Center(
-        child: TweenAnimationBuilder<double>(
-          tween: Tween<double>(begin: 0.0, end: 2.0),
-          duration: const Duration(milliseconds: 1500),
-          curve: Curves.easeOutCubic, // Curve tạo cảm giác mượt, chậm dần ở cuối
-          builder: (context, value, child) {
-            return Transform.scale(
-              scale: 0.8 + (0.6 * value), // Scale từ 0.8 lên 2.0
-              child: Opacity(
-                opacity: value / 2, // Fade từ 0 lên 1
-                child: child,
-              ),
-            );
-          },
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TweenAnimationBuilder<double>(
+              tween: Tween<double>(begin: 0.0, end: 1.0),
+              duration: const Duration(milliseconds: 1500),
+              curve: Curves.easeOutCubic,
+              onEnd: () {
+                setState(() {
+                  _showLoader = true;
+                });
+              },
+              builder: (context, value, child) {
+                return Transform.scale(
+                  scale: 0.8 + (1.2 * value),
+                  child: Opacity(opacity: value.clamp(0.0, 1.0), child: child),
+                );
+              },
+              child: Image.asset(
                 'assets/logo2_transparent.png',
                 width: 150,
                 height: 150,
                 fit: BoxFit.contain,
               ),
-            ],
-          ),
+            ),
+
+            const SizedBox(height: 80),
+
+            AnimatedOpacity(
+              opacity: _showLoader ? 1.0 : 0.0,
+              duration: const Duration(milliseconds: 500),
+              child: const SizedBox(
+                width: 28,
+                height: 28,
+                child: CircularProgressIndicator(strokeWidth: 3),
+              ),
+            ),
+          ],
         ),
       ),
     );
