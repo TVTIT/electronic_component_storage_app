8e9e964119c426a2676a616c818c14c5ce062e4f
TVTIT <trungtran200656@gmail.com>
2026-05-25 23:31:55 +0700

    chuyển splash sreen vào view

--- lib/view/auth/login_screen.dart
+++ lib/view/auth/login_screen.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:electronic_component_storage_app/control/supabase_account_controller.dart';
 import 'package:electronic_component_storage_app/control/supabase_database_controller.dart';
-import 'package:electronic_component_storage_app/splash_screen.dart';
+import 'package:electronic_component_storage_app/view/splash_screen.dart';
 import 'package:electronic_component_storage_app/view/my_app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
